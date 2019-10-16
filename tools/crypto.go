20db5c0f14cde70685083188b05bb9c0191572b9
buzzxu <xiang.leau@gmail.com>
2019-10-16 23:17:55 +0800

    md5()

---
+++ tools/crypto.go
@@ -0,0 +1,13 @@
+package tools
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// MD5 生成32位MD5
+func MD5(text string) string {
+	ctx := md5.New()
+	ctx.Write([]byte(text))
+	return hex.EncodeToString(ctx.Sum(nil))
+}
