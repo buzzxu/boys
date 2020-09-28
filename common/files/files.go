dd3b357b91ea1ff417b3c0b39b7c4f65414c32e3
buzzxu <xiang.leau@gmail.com>
2020-09-28 22:03:19 +0800

    add file func

--- common/files/files.go
+++ common/files/files.go
@@ -1,1 +1,24 @@
 package files
+
+import "os"
+
+// 判断所给路径文件/文件夹是否存在
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+// 判断所给路径是否为文件夹
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
+}
