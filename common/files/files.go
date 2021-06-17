16088b4ead602df651a96c82f8f25a05f7709ba2
buzzxu <xiang.leau@gmail.com>
2021-06-17 23:20:37 +0800

    ref: add copy file

--- common/files/files.go
+++ common/files/files.go
@@ -1,8 +1,11 @@
 package files
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
-// 判断所给路径文件/文件夹是否存在
+// Exists 判断所给路径文件/文件夹是否存在
 func Exists(path string) bool {
 	_, err := os.Stat(path)
 	if err != nil {
@@ -14,11 +17,26 @@ func Exists(path string) bool {
 	return true
 }
 
-// 判断所给路径是否为文件夹
+// IsDir 判断所给路径是否为文件夹
 func IsDir(path string) bool {
 	s, err := os.Stat(path)
 	if err != nil {
 		return false
 	}
 	return s.IsDir()
+}
+
+// Copy 复制文件
+func Copy(dstFile, srcFile string) (written int64, err error) {
+	src, err := os.Open(srcFile)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+	dst, err := os.OpenFile(dstFile, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+	return io.Copy(dst, src)
 }
---
+++ common/files/files_test.go
@@ -0,0 +1,7 @@
+package files
+
+import "testing"
+
+func TestCopy(t *testing.T) {
+	Copy("/Users/xux/tmp/bkm-server-1.0.0-SNAPSHOT.jar.--", "/Users/xux/tmp/bkm-server-1.0.0-SNAPSHOT.jar")
+}
