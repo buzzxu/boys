de4a296e0f705fec9e3617fd8bc8992c4d54c338
buzzxu <xiang.leau@gmail.com>
2023-12-26 10:02:37 +0800

    ref: a little

---
+++ types/optional.go
@@ -0,0 +1,39 @@
+package types
+
+// Optional 定义一个 Optional 类型。
+type Optional[T any] struct {
+	value T
+}
+
+// NewOptional 创建一个新的 Optional 对象。
+func NewOptional[T any](value T) Optional[T] {
+	return Optional[T]{value}
+}
+
+// IsEmpty 检查 Optional 对象是否为空。
+func (o Optional[T]) IsEmpty() bool {
+	return o.value == nil
+}
+
+// Get 获取 Optional 对象的值。
+func (o Optional[T]) Get() T {
+	if o.IsEmpty() {
+		panic("Optional is empty")
+	}
+	return o.value
+}
+
+// OrElseGet 如果 Optional 对象不为空，则返回其值，否则返回给定值。
+func (o Optional[T]) OrElseGet(value T) T {
+	if o.IsEmpty() {
+		return value
+	}
+	return o.value
+}
+
+// IfPresent 如果 Optional 对象不为空，则执行给定的消费者。
+func (o Optional[T]) IfPresent(consumer func(T)) {
+	if !o.IsEmpty() {
+		consumer(o.value)
+	}
+}
